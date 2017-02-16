2d1537613d90e1c8ae59a1e694534878e4a61c30
Alton Bodley <altonbodley@gmail.com>
2017-02-15 23:25:49 -0500

    Update listing.py

--- app/listing.py
+++ app/listing.py
@@ -3,7 +3,6 @@ import os
 def list_all():
     rootdir= os.getcwd()
     dir= rootdir+"/app/static/uploads"
-    print dir
     return os.listdir(dir)
     
     # for subdir, dirs, files in os.walk(rootdir+"/app/static/uploads"):
